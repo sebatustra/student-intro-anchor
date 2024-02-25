171e4b5d020c9958a75158c36650ca6110a4833e
sebatustra <sebatustra@protonmail.com>
2024-02-25 11:13:51 -0300

    added functionality to mint token when student intro is added. tests passing

--- programs/student-intro/src/lib.rs
+++ programs/student-intro/src/lib.rs
@@ -1,9 +1,12 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{associated_token::AssociatedToken, token::{Mint, Token, TokenAccount}};
 
 declare_id!("EZVdGWwx3FTorwSFJFWw12BZRdevcqro4vs1Vm9NTg7Q");
 
 #[program]
 pub mod student_intro {
+    use anchor_spl::token::{mint_to, MintTo};
+
     use super::*;
 
     pub fn initialize_intro(
@@ -18,6 +21,22 @@ pub mod student_intro {
         let intro = &mut ctx.accounts.student_intro;
         intro.name = name;
         intro.message = message;
+
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(), 
+                MintTo {
+                    authority: ctx.accounts.mint.to_account_info(),
+                    to: ctx.accounts.token_account.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info()
+                }, 
+                &[&[
+                    "mint".as_bytes(),
+                    &[ctx.bumps.mint]
+                ]]
+            ), 
+            10 * 10 ^ 6
+        )?;
 
         Ok(())
     }
@@ -41,6 +60,15 @@ pub mod student_intro {
     pub fn delete_intro(
         _ctx: Context<DeleteIntro>
     ) -> Result<()> {
+        msg!("Deleted intro");
+        Ok(())
+    }
+
+    pub fn initialize_mint(
+        _ctx: Context<InitializeMint>
+    ) -> Result<()> {
+        msg!("Initialized mint");
+
         Ok(())
     }
 
@@ -59,7 +87,23 @@ pub struct InitializeIntro<'info> {
     pub student_intro: Account<'info, StudentIntroState>,
     #[account(mut)]
     pub initializer: Signer<'info>,
-    pub system_program: Program<'info, System>
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    #[account(
+        seeds = ["mint".as_bytes()],
+        bump,
+        mut
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = initializer,
+        associated_token::mint = mint,
+        associated_token::authority = initializer
+    )]
+    pub token_account: Account<'info, TokenAccount>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>
 }
 
 #[derive(Accounts)]
@@ -90,6 +134,24 @@ pub struct DeleteIntro<'info> {
     pub student_intro: Account<'info, StudentIntroState>,
     #[account(mut)]
     pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct InitializeMint<'info> {
+    #[account(
+        init,
+        seeds = ["mint".as_bytes()],
+        bump,
+        payer = user,
+        mint::decimals = 6,
+        mint::authority = mint
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
     pub system_program: Program<'info, System>
 }
 
--- tests/student-intro.ts
+++ tests/student-intro.ts
@@ -2,6 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { StudentIntro } from "../target/types/student_intro";
 import { expect } from "chai";
+import { getAssociatedTokenAddress, getAccount } from "@solana/spl-token";
 
 describe("student-intro", () => {
     // Configure the client to use the local cluster.
@@ -20,9 +21,24 @@ describe("student-intro", () => {
         program.programId
     );
 
+    const [mint] = anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from("mint")],
+        program.programId
+    );
+
+    it("Initialized the token mint", async () => {
+        const tx = await program.methods
+            .initializeMint()
+            .rpc();
+    })
+
+
     it("Initialized the student intro", async () => {
+        const tokenAccount = await getAssociatedTokenAddress(mint, provider.wallet.publicKey);
+
         const tx = await program.methods
             .initializeIntro(studentIntro.name, studentIntro.message)
+            .accounts({tokenAccount})
             .rpc();
         
         const intro = await program.account
@@ -31,6 +47,10 @@ describe("student-intro", () => {
 
         expect(intro.name === studentIntro.name);
         expect(intro.message === studentIntro.message);
+
+        const userAta = await getAccount(provider.connection, tokenAccount);
+
+        expect(Number(userAta.amount)).to.equal((10* 10) ^ 6);
     })
 
     it("Updated the student intro", async () => {
